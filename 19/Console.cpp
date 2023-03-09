ec64d855f6cb145491c5762081e1bd6f8a0c13c2
gwak1635 <echo3a3b@naver.com>
2023-03-09 13:10:06 +0900

    19강

--- 19/Console.cpp
+++ 19/Console.cpp
@@ -3,6 +3,8 @@
 
 #include "stdafx.h"
 
+
+
 struct tagMonster { //몬스터 구조체
     string  strName;
     int     nHp;
@@ -13,6 +15,9 @@ struct tagMonster { //몬스터 구조체
 
 int main()
 {
+    srand(time(NULL));
+    rand();
+
     tagMonster stMonster; //구조체 선언 
 
     //구조체 값 설정
@@ -22,9 +27,58 @@ int main()
     stMonster.nAtt = 10;
     stMonster.nDef = 5;
     
-    cout << stMonster.strName.c_str() << "의 공격력은" << stMonster.nAtt << "이다." << endl;
+    cout << stMonster.strName.c_str() << "의 공격력은 " << stMonster.nAtt << "이다." << endl;   
 
+    tagMonster stMonster1 = { "뱀파이어",50,100,25,0 };//구조체 선언 및 초기화
 
+    cout << stMonster1.strName.c_str() << "의 공격력은 " << stMonster1.nAtt << "이다." << endl;
     
+    tagMonster* pMonster;
+    pMonster = new tagMonster;
+
+    //pMonster = 주소
+    //pMonster->strName
+    //pMonster[0].strName = 값
+    //*pMonster.strName 이건가? 아! 연산자!
+    pMonster[0].strName = "트롤";
+    pMonster->nHp = 100;
+    (*pMonster).nMp = 100;
+    pMonster->nAtt = 100;
+    pMonster->nDef = 100;
+
+    cout << pMonster->strName.c_str() << "의 공격력은 " << pMonster[0].nAtt << "이다." << endl;
+
+    delete pMonster;
+
+    tagMonster stMonsterArray[10];
+    for (int i = 0; i < 10; i++) {
+        stMonsterArray[i].strName = "오우거";
+        stMonsterArray[i].nHp = 100 + rand() % 51;
+        stMonsterArray[i].nMp = 30;
+        stMonsterArray[i].nAtt = 10;
+        stMonsterArray[i].nDef = 5;
+    }
+
+    //배열에 대한 동적할당
+
+    tagMonster* aMonster = new tagMonster[10];
+    aMonster[0].strName = "뱀파이어1";
+    aMonster[1].strName = "뱀파이어2";
+    delete[] aMonster;
+
+    tagMonster* aMonster2;
+    int nNumOfMon = 10 + rand() % 11;
+    aMonster2 = new tagMonster[nNumOfMon];
+
+    for (int i = 0; i < nNumOfMon; i++)
+    {
+        aMonster2[i].strName = "던전형 몬스터";
+        aMonster2[i].nHp = 100 + rand() % 51;
+        aMonster2[i].nMp = 30;
+        aMonster2[i].nAtt = 10; +rand % 5;
+        aMonster2[i].nDef = 5;
+
+    }
+
     return 0;
 }
